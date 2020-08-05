38b5fad444f0493c1ceb64ec538dc33b6e5815c2
zcz； <zhangchangzhen@meituan.com>
2020-08-05 14:14:18 +0800

    enviroment property

--- spring-annotation-ouyangxizhu/src/main/java/com/ouyangxizhu/MainTestValuePropertySource.java
+++ spring-annotation-ouyangxizhu/src/main/java/com/ouyangxizhu/MainTestValuePropertySource.java
@@ -3,11 +3,18 @@ package com.ouyangxizhu;
 import com.ouyangxizhu.bean.PersonValue;
 import com.ouyangxizhu.config.MainConfigValuePropertySource;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MutablePropertySources;
 
 public class MainTestValuePropertySource {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MainConfigValuePropertySource.class);
         PersonValue personValue = (PersonValue) context.getBean("personValue");
         System.out.println(personValue);
+        System.out.println("============");
+        ConfigurableEnvironment environment = context.getEnvironment();
+        String property = environment.getProperty("personValue.nickName");
+        System.out.println(property);
+
     }
 }
